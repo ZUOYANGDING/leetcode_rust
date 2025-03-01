aa0ca95bcdeba00b1d56ac65379089a3e803af79
zuoyangding <zuoyangding@hotmail.com>
2025-03-01 21:33:05 +0800

    full knapsack with 1d dp array

--- topic_dp/full_knapsack/src/main.rs
+++ topic_dp/full_knapsack/src/main.rs
@@ -24,6 +24,25 @@ impl Solution {
         }
         dp[items.len() - 1][knapsack as usize]
     }
+
+    fn full_knapsack_1d_dp_array(items: Vec<i32>, values: Vec<i32>, knapsack: i32) -> i32 {
+        assert!(items.len() == values.len());
+
+        let mut dp = vec![0; (knapsack + 1) as usize];
+
+        // init
+        for idx in items[0]..=knapsack {
+            dp[idx as usize] = dp[(idx - items[0]) as usize] + values[0];
+        }
+
+        for i in 1..items.len() {
+            for j in items[i] as usize..=knapsack as usize {
+                dp[j] = std::cmp::max(dp[j], dp[j - items[i] as usize] + values[i]);
+            }
+            println!("{:?}", dp);
+        }
+        dp[knapsack as usize]
+    }
 }
 fn main() {
     let mut buf = String::new();
@@ -57,5 +76,12 @@ fn main() {
     assert!(values.len() == nums[0] as usize);
     buf.clear();
 
-    println!("{:?}", Solution::full_knapsack(items, values, nums[1]));
+    println!(
+        "{:?}",
+        Solution::full_knapsack(items.clone(), values.clone(), nums[1])
+    );
+    println!(
+        "{:?}",
+        Solution::full_knapsack_1d_dp_array(items, values, nums[1])
+    )
 }
